53ac02dfb5374d55f03ea000e1a1153702749803
Michael <michael.github@eipsoftware.com>
2019-03-20 17:48:18 -0400

    Add project files.

---
+++ ISM6225_Assignment_3_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ISM6225_Assignment_3_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
